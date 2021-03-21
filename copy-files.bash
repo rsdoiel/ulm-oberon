55acb22eb0fa96e5c340978d4bf7c3ead69beffd
R. S. Doiel <=>
2021-03-21 10:33:52 -0700

    Initial Archive

---
+++ copy-files.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget "http://www.mathematik.uni-ulm.de/oberon/ftp/i386/READ_ME"
+wget "http://www.mathematik.uni-ulm.de/oberon/ftp/i386/examples/ReverseIPv4.od"
+wget "http://www.mathematik.uni-ulm.de/oberon/ftp/i386/examples/ReverseIPv4.om"
+wget "http://www.mathematik.uni-ulm.de/oberon/ftp/i386/tof2elf"
+wget "http://www.mathematik.uni-ulm.de/oberon/ftp/i386/ulm-oberon-i386-0.8.10.tar.gz"
+
